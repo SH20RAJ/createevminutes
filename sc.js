8eaff6e032da273c7abb40bbe6b1289b2b60107b
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-01-11 17:52:08 +0000

    play site added - home page added

--- sc.js
+++ sc.js
@@ -4,20 +4,34 @@ let video = document.querySelector('#video');
 let title = document.querySelector('#title');
 
 
-video.src = videojson.videos[0].videourl;
 window.currentvideo = 0;
 
-
 //Functions 
+function init(){
+  video.src = videojson.videos[0].videourl;
+  title.innerHTML = videojson.videos[0].title;
+  
+}
+
 
 function next(){
+  if(currentvideo == 10) back();
   let id = videojson.videos[currentvideo + 1];
   video.src = id.videourl;
   title.innerHTML = id.title;
   currentvideo = currentvideo + 1;
+  fill();
 }
 
-//Onclicks
+function prev(){
+  if(currentvideo == 1) back();
+  let id = videojson.videos[currentvideo - 1];
+  video.src = id.videourl;
+  title.innerHTML = id.title;
+  currentvideo = currentvideo - 1;
+  fill();
+}
+
 function playpause() {
   if (video.paused){
     video.play();
@@ -25,3 +39,19 @@ function playpause() {
     video.pause();
   }
 }
+
+function fill(){
+  let x = currentvideo/10 * 100;
+  let fill = document.querySelector('#completed').style.width = ""+x+"%";
+  document.location.hash = currentvideo;
+  check();
+}
+
+function check(){
+  
+  
+}
+function back(){
+  window.location.href = "feeds.html"
+}
+
